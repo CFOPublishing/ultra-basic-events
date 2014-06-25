eacac2767a1c1e520e62d00db76369417b48fa76
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-06-25 15:15:15 -0400

    Establishing events schema

--- includes/schema.php
+++ includes/schema.php
@@ -1,2 +1,57 @@
 <?php
 
+class UBEP_Schema {
+    
+	public function init() {
+		static $instance;
+
+		if ( empty( $instance ) ) {
+			$instance = new self;
+		}
+
+		return $instance;
+	}	
+
+	public function __construct() {
+		$this->post_type = 'ubep_simple_event';
+        $this->date_meta = 'ubep_event_date';
+        add_action( 'init', array( $this, 'register_feed_post_type' ) );
+    }
+    
+	/**
+	 * All we need is a basic custom post type. Let's not get crazy.
+	 */
+	public function register_feed_post_type() {
+		$labels = array(
+			'name'               => __( 'Events', 'ubep' ),
+			'singular_name'      => __( 'Event', 'ubep' ),
+			'add_new'            => _x( 'Add New', 'ubep', 'add new event' ),
+			'all_items'          => __( 'All Events', 'ubep' ),
+			'add_new_item'       => __( 'Add New Event', 'ubep' ),
+			'edit_item'          => __( 'Edit Event', 'ubep' ),
+			'new_item'           => __( 'New Event', 'ubep' ),
+			'view_item'          => __( 'View Event', 'ubep' ),
+			'search_items'       => __( 'Search Events', 'ubep' ),
+			'not_found'          => __( 'No events found', 'ubep' ),
+			'not_found_in_trash' => __( 'No events found in trash', 'ubep' ),
+		);
+
+		register_post_type( $this->post_type, apply_filters( 'ubep_register_feed_post_type_args', array(
+			'label'       => $labels['name'],
+			'labels'      => $labels,
+			'description' => __( 'Events created by the Ultra Basic Events plugin', 'ubep' ),
+			'public'      => true,
+			'hierarchical' => false,
+            'publicly_queryable' => true,
+            'exclude_from_search' => true,
+			'supports' 	=> array('title','editor','author','thumbnail','excerpt','custom-fields','page-attributes'),
+			'taxonomies' => array('category', 'post_tag'),
+            'capabilities' => array('edit_posts')
+			#'show_in_menu' => ubep()->menu_slug
+			#'menu_position' => 100
+			#'show_ui'     => true, // for testing only
+		) ) );
+
+		do_action( 'ubep_feed_post_type_registered' );
+	}    
+}
--- ultra-basic-events.php
+++ ultra-basic-events.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+Plugin Name: Ultra Basic Events Plugin
+Plugin URI: http://cfo.com/
+Description: Because all you want is a list of events and descriptions ordered by when they will happen, and not that other junk.
+Version: 0.0.1
+Author: Aram Zucker-Scharff
+Author URI: http://aramzs.me/
+License: GPL2
+*/
+
+/*  Developed for the CFO Publishing LLC
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
 class Ultra_Basic_Events_Plugin {
 
     var $slug;
