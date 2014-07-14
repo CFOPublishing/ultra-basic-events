dc0ef839d6cc092fd10af298fa944c5f04df65cc
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-07-14 15:41:53 -0400

    Putting the result into the value field.

--- assets/js/datepicker-imp.js
+++ assets/js/datepicker-imp.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function(){
     
-    jQuery( ".date-time-pick-zs-util" ).datepicker().formatDate("@");
+    jQuery( ".date-time-pick-zs-util" ).datepicker(
+        {
+            onSelect: function(dateText, inst)
+            {
+                console.log(this);
+                var dateObject = jQuery(this).datepicker('getDate');
+                console.log(dateObject);
+                //var unixDate = jQuery(this).datepicker.formatDate("@", new Date(dateObject));
+                //console.log(unixDate);
+                jQuery(this).attr('value', dateObject);
+            },
+            attributeBindings: ["value"]
+        }
+    );
+    //console.log(datetime_pick_obj.datepicker("getDate"));
+    //jQuery( ".date-time-pick-zs-util" ).val(datetime_pick_obj.datepicker("getDate").formatDate('@'));
     
 });
